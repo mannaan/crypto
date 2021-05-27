55b11ced2f7681595827f046496c7d60b00dc664
Abdul Manan <abdul.mannaan@gmail.com>
2021-05-27 22:37:01 +0100

    Implemented Encryption/Decryption methods (all tests pass)

--- src/Crypto/Crypto.Cipher/ShiftCipherObfuscator.cs
+++ src/Crypto/Crypto.Cipher/ShiftCipherObfuscator.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Text;
 
 namespace Crypto.Cipher
 {
@@ -10,14 +10,37 @@ namespace Crypto.Cipher
         {
             _shiftKey = shiftKey;
         }
+
         public string Encode(string text)
         {
-            throw new NotImplementedException();
+            return Shift(text, _shiftKey);
         }
 
         public string Decode(string cipher)
         {
-            throw new NotImplementedException();
+            return Shift(cipher, 26 - _shiftKey);
+        }
+        private string Shift(string text, int shiftBy)
+        {
+            if (string.IsNullOrEmpty(text))
+                return string.Empty;
+            var shiftedTextBuilder = new StringBuilder();
+            foreach (var character in text)
+            {
+                if (char.IsUpper(character))
+                {
+                    shiftedTextBuilder.Append(Shift(character, shiftBy, 'A'));
+                }
+                else if (char.IsLower(character))
+                {
+                    shiftedTextBuilder.Append(Shift(character, shiftBy, 'a'));
+                }
+            }
+            return shiftedTextBuilder.ToString();
+        }
+        private char Shift(char alphabet, int shiftBy, char alphabetBaseLine)
+        {
+            return (char)((alphabet + shiftBy - alphabetBaseLine) % 26 + alphabetBaseLine);
         }
     }
 }
